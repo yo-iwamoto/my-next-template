1f141ca2dd86a26d9d9483d30d9aa84e93e66c92
you-5805 <you.iwamoto918@gmail.com>
2022-07-02 23:42:20 +0900

    add: .handler.ts as pageExtensions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  pageExtensions: ['page.tsx'],
+  pageExtensions: ['page.tsx', 'handler.ts'],
   swcMinify: true,
   experimental: {
     outputStandalone: true,
