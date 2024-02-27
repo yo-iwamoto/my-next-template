0e9c44c836b2a127052cec76281c64cc2135d982
Yo Iwamoto <you.iwamoto918@gmail.com>
2024-02-28 06:15:05 +0900

    fix: vitest path alias config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/": "src/",
+      "@": path.resolve(__dirname, "src"),
     },
   },
   test: {
