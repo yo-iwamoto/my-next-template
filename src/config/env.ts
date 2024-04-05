034acf8e4e194aa74b58c7b0507761d14866c36e
Yo Iwamoto <you.iwamoto918@gmail.com>
2024-04-05 20:24:26 +0900

    remove @ts-oss/env-nextjs

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,24 @@
-import { createEnv } from "@t3-oss/env-nextjs";
+import { cache } from "react";
+import { z } from "zod";
 
-export const envVars = createEnv({
-  server: {},
-  client: {},
-  runtimeEnv: {},
+const serverSideEnvSchema = z.object({
+  // API_TOKEN: z.string(),
 });
+
+function getServerSideEnvFn() {
+  return serverSideEnvSchema.parse({
+    // API_TOKEN: process.env.API_TOKEN,
+  });
+}
+
+export const getServerSideEnv = cache(getServerSideEnvFn);
+
+const clientSideEnvSchema = z.object({
+  // NEXT_PUBLIC_API_URL: z.string(),
+});
+
+export function getClientSideEnv() {
+  return clientSideEnvSchema.parse({
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  });
+}
