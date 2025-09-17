888e1905866a0ddfe211589059f2da680fedbe9c
Yo Iwamoto <you.iwamoto918@gmail.com>
2025-09-17 22:02:26 +0900

    add defaultLocale

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -2,6 +2,7 @@ import { createNavigation } from "next-intl/navigation";
 import { locales } from "../i18n/locales/list";
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation({
+  defaultLocale: "ja",
   locales,
   localePrefix: "as-needed",
 });
