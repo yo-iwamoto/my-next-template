e407132d2e0b6d7b8fb4267ea5f6529424b203fe
Yo Iwamoto <you.iwamoto918@gmail.com>
2026-03-29 11:53:22 +0900

    chore: upgrade deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,10 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
